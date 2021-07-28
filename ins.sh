4f0edf5d9bdf8b3903de9ca3bd9d3c9aafd42100
nou <83199729+nooufiy@users.noreply.github.com>
2021-07-28 08:09:37 +0700

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -2,15 +2,12 @@
 
 yum install -y gcc-c++ make 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
-
 yum install nodejs -y
-
 curl -o- -L https://yarnpkg.com/install.sh | bash
-
 npm install -g auto-install
-
 curl -sO https://raw.githubusercontent.com/nooufiy/gnod/main/sv.js            #silent&createfile
-
+ip=$(hostname -I)
+sed -i "s/127.0.0.1/$ip/g" sv.js
 node -v
 npm -v
 
