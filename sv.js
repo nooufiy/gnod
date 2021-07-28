b31f7f6b100a1f504617fd7eb5e142a9e57a4fd7
nou <83199729+nooufiy@users.noreply.github.com>
2021-07-29 05:50:25 +0700

    Update sv.js

--- sv.js
+++ sv.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('hi bro.. Nodejs');
-}).listen(3001, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3001/');
+  res.end('hi bro..');
+}).listen(3003, 'ipserver');
+console.log('Server running at http://ipserver:3003/');
